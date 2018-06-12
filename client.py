3ccfa852a088ae13a4f75be3a79ab4851e8f25d2
Ruslan Tyukaev <tyukaevruslan@gmail.com>
2018-06-12 17:02:44 +0400

    First commit

---
+++ client.py
@@ -0,0 +1,10 @@
+import socket
+
+sock = socket.socket()
+sock.connect(('localhost', 8080))
+sock.send('some data'.encode())
+
+data = sock.recv(1024)
+sock.close()
+
+print (data)
---
+++ server.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import socket
+
+sock = socket.socket()
+sock.bind(('', 8080))
+sock.listen(1)
+conn, addr = sock.accept()
+
+print ('connected:'+str(addr))
+
+try:
+
+	while True:
+		data = conn.recv(1024)
+		if not data:
+			break
+		conn.send(data.upper())
+except Exception:
+	print ("Error:" + str(Exception))
+	
+print("Connect closed")
+conn.close()

ceb8492e2c0880028072f5d5ecb3bbccca60f28c
Ruslan Tyukaev <tyukaevruslan@gmail.com>
2018-06-12 19:01:21 +0400

    rem exception

--- ping.py
+++ ping.py
@@ -6,21 +6,16 @@ import time
 def tcping(host, port=80, timeout=2):
     s = socket.socket()
     s.settimeout(timeout)
-    result = False
-    end = None
     try:
         start = time.time()
         s.connect((host, port))
         s.close()
-        result = True
         end = time.time()
     except Exception as e:
         if e[0] == errno.ECONNREFUSED:
-            result = True
-    end = time.time()
+            end = time.time()
     ms = 1000*(end-start)
-    print (ms)
-    return result
+    print (str(ms) +" ms")
 
 if __name__ == '__main__':
     times = 3 
